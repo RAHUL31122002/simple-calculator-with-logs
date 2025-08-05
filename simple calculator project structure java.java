f0f0fd003bb58696e4630e4eee2cdd12a09e7bc2
RAHUL KUMAR <rahul31122002@gmail.com>
2025-08-05 21:36:40 +0530

    Add files via upload

---
+++ simple calculator project structure java.java
@@ -0,0 +1,13 @@
+calculator/
+├── src/
+│   └── main/
+│       ├── java/com/example/calculator/
+│       │   ├── C:\Users\DELL\Desktop\calculatorapplication.java
+│       │   ├── controller/C:\Users\DELL\Desktop\CalculatorController.java
+│       │   ├── model/C:\Users\DELL\Desktop\CalculationLog.java
+│       │   └── repository/C:\Users\DELL\Desktop\CalculationLogRepository.java
+│       └── resources/
+│           ├── C:\Users\DELL\Desktop\application.properties
+│           └──   C:\Users\DELL\Desktop\simple calculator with logs index.html  ← Frontend goes here
+└── C:\Users\DELL\Desktop\pom.xml
+
